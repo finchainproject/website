d57f7ed3494f2c74463e1d189b83b9ae44e4976f
PresslogicSimon <simon@presslogic.com>
2018-02-07 21:29:29 +0800

    init version 2

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+const sassMiddleware = require('node-sass-middleware')
 
-
-/* wordings */ 
-// const langCN = require('./lang/cn')
 
 app.set('view engine', 'pug')
+
 
 app.get('/', (req, res) => {
   res.redirect('/cn')
@@ -21,12 +21,25 @@ app.get('/en', (req, res) => {
   res.render('index', { currentLang: 'en', langText: langText })
 })
 
+app.use(
+  sassMiddleware({
+      src: './asset', 
+      dest: './public',
+      debug: true,
+      outputStyle: 'compressed'
+  })
+);   
+
+
 app.use(express.static('public'))
 app.use('/images', express.static('images'))
 
 app.use(function(req, res, next){
   res.redirect('/cn')
 });
+
+
+
 
 let serverPORT = process.env.PORT || 3000;
 
