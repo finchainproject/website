b698d7cc49857e4fb13d4d2d1667ad04c5f3673c
PresslogicSimon <simon@presslogic.com>
2018-02-09 18:04:34 +0800

    support mobile site

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const sassMiddleware = require('node-sass-middleware')
+const MobileDetect = require('mobile-detect')
 
 
 app.set('view engine', 'pug')
-
 
 app.get('/', (req, res) => {
   res.redirect('/cn')
@@ -13,19 +13,29 @@ app.get('/', (req, res) => {
 
 app.get('/cn', (req, res) => {
   let langText = require('./lang/cn')
-  res.render('index', { currentLang: 'cn', langText: langText })
+  let md = new MobileDetect(req.headers['user-agent'])  
+  if( !md.mobile()  ){
+    res.render('desktop/index', { currentLang: 'cn', langText: langText })
+  }else{
+    res.render('mobile/index', { currentLang: 'cn', langText: langText })
+  }
 })
 
 app.get('/en', (req, res) => {
   let langText = require('./lang/en')
-  res.render('index', { currentLang: 'en', langText: langText })
+  let md = new MobileDetect(req.headers['user-agent'])  
+  if( !md.mobile()  ){
+    res.render('desktop/index', { currentLang: 'en', langText: langText })
+  }else{
+    res.render('mobile/index', { currentLang: 'en', langText: langText })
+  }
 })
 
 app.use(
   sassMiddleware({
       src: './asset', 
       dest: './public',
-      debug: true,
+      debug: false,
       outputStyle: 'compressed'
   })
 );   
