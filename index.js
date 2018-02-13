9e266b566d1926631b1ea85546ac393f1c11ab5f
PresslogicSimon <simon@presslogic.com>
2018-02-13 17:39:09 +0800

    use i18n-express for supporting multi-lang

--- index.js
+++ index.js
@@ -3,31 +3,48 @@ const app = express()
 const path = require('path')
 const sassMiddleware = require('node-sass-middleware')
 const MobileDetect = require('mobile-detect')
+const i18n = require('i18n-express')
+const cookieParser = require('cookie-parser')
 
 
 app.set('view engine', 'pug')
+app.use(cookieParser())
 
-app.get('/', (req, res) => {
-  res.redirect('/cn')
+app.use('/:lang', (req, res, next) => {
+  if(req.params.lang == 'cn' || req.params.lang == 'en') {
+    req.cookies.ulang = req.params.lang
+    next()
+  }else{
+    next()
+  }
 })
 
+app.use(i18n({
+  translationsPath: path.join(__dirname, 'i18n'), 
+  siteLangs: ["cn","en"],
+  textsVarName: 'langText',
+  defaultLang: 'cn'
+}));
+
+// app.get('/', (req, res) => {
+//   res.redirect('/cn')
+// })
+
 app.get('/cn', (req, res) => {
-  let langText = require('./lang/cn')
   let md = new MobileDetect(req.headers['user-agent'])  
   if( !md.mobile()  ){
-    res.render('desktop/index', { currentLang: 'cn', langText: langText })
+    res.render('desktop/index', { currentLang: 'cn' })
   }else{
-    res.render('mobile/index', { currentLang: 'cn', langText: langText })
+    res.render('mobile/index', { currentLang: 'cn' })
   }
 })
 
 app.get('/en', (req, res) => {
-  let langText = require('./lang/en')
   let md = new MobileDetect(req.headers['user-agent'])  
   if( !md.mobile()  ){
-    res.render('desktop/index', { currentLang: 'en', langText: langText })
+    res.render('desktop/index', { currentLang: 'en' })
   }else{
-    res.render('mobile/index', { currentLang: 'en', langText: langText })
+    res.render('mobile/index', { currentLang: 'en' })
   }
 })
 
@@ -44,11 +61,9 @@ app.use(
 app.use(express.static('public'))
 app.use('/images', express.static('images'))
 
-app.use(function(req, res, next){
-  res.redirect('/cn')
-});
-
-
+app.use(function(req, res, next){ 
+  res.redirect('/cn') 
+}); 
 
 
 let serverPORT = process.env.PORT || 3000;
